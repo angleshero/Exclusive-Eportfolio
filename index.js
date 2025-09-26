f9729ef4290d332c61b75652030d3e34f79430eb
Tommy <tommylcowart@gmail.com>
2025-09-26 10:26:37 -0500

    update again

--- index.js
+++ index.js
@@ -6,21 +6,29 @@ function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading')
       const success = document.querySelector('.modal__overlay--sucess')
+
+      //Show loading
+      loading.classList.add("modal__overlay--visible");
+
     emailjs
        .sendForm(
-             'service_h1axg39',
+             'service_00mf36u',
              'template_trn1jkn',
               event.target,
               'I2MhMNPK4zmqBk9ul'
-        ).then(() => {
+        )
+        .then((response) => {
+            console.log("SUCCESS!", response.status, response.text);
             loading.classList.remove("modal__overlay--visible");
-        success.classList += " modal__overlay--visible";
+            success.classList += " modal__overlay--visible";
             
-       }).catch(() => {
+       })
+       .catch((error) => {
+        console.error("FAILED...", error);
         loading.classList.remove("modal__overflow--visible");
         alert(
             "The email service is temporarily unavailable. Please contact me directly on tommylcowart@gmail.com"
         );
-       })
+       });
       
           }
